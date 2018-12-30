2477ad8990716b00e123664ff924eb42adcf4691
Rostyslav Haidukov <rostyslav.haidukov@inventorsoft.co>
2018-12-30 19:25:36 +0200

    initial commit

---
+++ api/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { PORT, MONGODB_URI } = require('./config');
+const app = express();
+
+const server = http.createServer(app);
+
+mongoose.Promise = Promise;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+server.listen(PORT, () => {
+    console.log(`App is listening on ${server.address().port}`);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error(`Unhandled rejection at: ${promise}`);
+    console.error(`Reason: ${reason}`)
+})
---
+++ api/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    MONGODB_URI: process.env.PORT || 'mongodb://rostyslav7:rostyk99@ds145474.mlab.com:45474/tasker_db'
+}
