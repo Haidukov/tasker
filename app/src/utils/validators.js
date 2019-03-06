25a0aba59ecf3a546b14d7d9fc0f8b115a0ac614
Haidukov <rostyk.haidukov1999@gmail.com>
2019-03-07 01:33:12 +0200

    removed file

--- app/src/utils/validators.js
+++
@@ -1,8 +0,0 @@
-import { ValidatorForm } from 'react-material-ui-form-validator';
-
-ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
-    if (value !== this.state.form.password) {
-        return false;
-    }
-    return true;
-});
