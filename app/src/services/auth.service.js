3629fa7272dc5d4a80456155b33de598f68a20cb
Haidukov <rostyk.haidukov1999@gmail.com>
2019-03-06 23:38:22 +0200

    added request interceptor

--- app/src/services/auth.service.js
+++ app/src/services/auth.service.js
@@ -3,10 +3,15 @@ import { setTokenToLocalStorage } from './local-storage.service';
 
 export function login(form) {
     return Http.post('/public/login', form)
-        .then(({ data }) => setTokenToLocalStorage(data));
+        .then(({ data }) => setTokenToLocalStorage(data))
+        .then(() => fetchUserData())
 }
 
 export function signUp(form) {
     return Http.post('/public/sign-up', form)
         .then(() => login(form));
+}
+
+export function fetchUserData() {
+    return Http.get('/user-data');
 }
--- app/src/utils/http.js
+++ app/src/utils/http.js
@@ -1,7 +1,21 @@
 import axios from 'axios';
+import { getTokenFromLocalStorage } from '../services/local-storage.service';
 
 export const Http = axios.create({
     baseURL: 'http://localhost:8000/api',
     contentType: 'application/json'
 });
+
+Http.interceptors.request.use(
+    request => {
+        if (!request.url.includes('/public')) {
+            const { accessToken } = getTokenFromLocalStorage();
+            request.headers = {
+                ...request.headers,
+                Authorization: `Bearer ${accessToken}`
+            };
+        }
+        return request;
+
+}, error => error);
 
